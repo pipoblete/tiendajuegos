08945c529741188f07d9245f91cafe7b19e49d93
pipoblete <127996548+pipoblete@users.noreply.github.com>
2024-03-25 23:46:14 -0300

    se agregó modelo de juegos

--- tiendajuegos/juegos/models.py
+++ tiendajuegos/juegos/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Juego(models.Model):
+    nombre = models.CharField(max_length=100)
+    descripcion = models.TextField()
+    categoria = models.CharField(max_length=50)
+    imagen = models.ImageField(upload_to='juegos/', null=True, blank=True)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+
+    def __str__(self):
+        return self.nombre
