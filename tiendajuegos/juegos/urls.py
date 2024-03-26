07d5fbb48ec196f58ca5bb617e00975e3924cd36
pipoblete <127996548+pipoblete@users.noreply.github.com>
2024-03-25 22:47:18 -0300

    registro e inicio de sesión

--- tiendajuegos/juegos/urls.py
+++ tiendajuegos/juegos/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import index
+from .views import index, registro, login
 
 urlpatterns = [
-    path('juegos', index, name='juegos')
+    path('inicio', index, name='inicio'),
+    path('registro', registro, name='registro'),
+    path('login', login, name='login'),
+    
 ]
